6182afcdfb541696eb494c85a346e31c071f4154
dsmdurga00 <84117977+dsmdurga00@users.noreply.github.com>
2025-07-07 13:24:18 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,53 @@
+package com.weather.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import com.weather.dao.UserDaoImpl;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static UserDaoImpl userDao = new UserDaoImpl();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String username = request.getParameter("username"); 
+        String password = request.getParameter("password");
+
+        if (userDao.isValidUser(username, password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            response.sendRedirect("index1.html");
+        }
+        
+        else
+       {
+            response.sendRedirect("index1.html?error=1");
+        	System.out.println("Error A gya");
+        }
+		
+	}
+	}
+	
+
+
---
+++ RegisterServlet.java
@@ -0,0 +1,51 @@
+package com.weather.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.weather.dao.User;
+import com.weather.dao.UserDaoImpl;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static UserDaoImpl userDao = new UserDaoImpl();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        UserDaoImpl userDao = new UserDaoImpl();
+        if (userDao.addUser(user)) {
+            response.sendRedirect("login.jsp?registration=success");
+        } else {
+            response.sendRedirect("login.jsp?error=1");
+        }
+		
+	}
+
+}
